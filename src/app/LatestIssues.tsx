95b6cd4d9efe9ae64eb47480fac9142400b389e7
Fawad Soomro <fawadsoomro877@gmail.com>
2024-06-13 02:55:30 +0500

    Build the IssueSummary component

--- src/app/LatestIssues.tsx
+++ src/app/LatestIssues.tsx
@@ -2,7 +2,7 @@ import IssueStatusBadge from "@/components/IssueStatusBadge";
 import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
-
+import prisma from "../../prisma/client";
 const LatestIssues = async () => {
   const issues = await prisma?.issue.findMany({
     orderBy: { createdAt: "desc" },
